507330ca452174f592eeae49f72c3051b493067b
@omkarmore <omkarmore40@gmail.com>
2021-09-23 11:15:42 +0530

    new code

--- main.py
+++ main.py
@@ -11,9 +11,3 @@ driver.get("https://www.google.com/")
 driver.find_element_by_xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input").send_keys("python")
 time.sleep(0.5)
 driver.find_element_by_name("btnK").click()
-fhbcfhbchbdb
-fhdfh
-dfhd
-dfhd
-dfhd
-fh
